a0367508c5aad90569595b943b1b40fae6f0a5a6
GBelonozhko <44050866+GBelonozhko@users.noreply.github.com>
2020-10-24 18:10:39 -0700

    master

---
+++ build/precache-manifest.bfb6be232fb96405b84f124c04643e67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94541b3a18372c32ada85d23b8825d99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6e3178299d18773d372",
+    "url": "/static/css/main.a68f76ae.chunk.css"
+  },
+  {
+    "revision": "e5bc206500f44d2516f9",
+    "url": "/static/js/2.9d5046a0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.9d5046a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6e3178299d18773d372",
+    "url": "/static/js/main.c345763f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
